032ed1cbfec16224fda2ca43e106a02ea8506add
freestyleabg <andre.b.grant>
2023-08-18 02:45:32 +0100

    Fix bug with duplicate characters and infinite loop

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -14,13 +14,16 @@ class Hangman:
 
     def check_guess(self, guess):
         self.guess = guess
+        found = False
         if guess.lower() in self.word.lower():
-            print(f"Good guess! {guess} is in the word.")
-            for letter in self.word:
+            for index, letter in enumerate(self.word):
                 if letter == guess:
-                    self.word_guessed[self.word.index(guess)] = guess
-                    print(self.word_guessed)
+                    found = True
+                    self.word_guessed[index] = guess
             self.num_letters -= 1
+        if found:
+            print(f"Good guess! {guess} is in the word.")
+            print(self.word_guessed)
         else:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
@@ -28,23 +31,26 @@ class Hangman:
     def ask_for_input(self):
         while True:
             guess = input("Enter a single letter: ").lower()
-            if guess.isalpha() is False and len(guess) > 1:
+            if guess.isalpha() and len(guess) == 1 and guess not in self.list_of_guesses:
+                break
+            elif not guess.isalpha() or len(guess) != 1:
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif any(guess in element for element in self.list_of_guesses):
+            elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
-            else:
-                self.check_guess(guess)
-                self.list_of_guesses.append(guess)
+
+        self.check_guess(guess)
+        self.list_of_guesses.append(guess)
 def play_game(word_list_entry):
     num_lives=5
     game = Hangman(word_list_entry, num_lives)
+
     while True:
         if game.num_lives == 0:
             print("You lost!")
             break
-        if game.num_letters > 0:
+        elif game.num_letters > 0:
             game.ask_for_input()
-        if game.num_lives != 0 and not game.num_letters >= 1:
+        else:
             print("Congratulations. You won the game!")
             break
 
