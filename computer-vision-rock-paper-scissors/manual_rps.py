3f31d86340f3644a9a23412eb608832ad0d33bb8
freestyleabg <andre.b.grant@gmail.com>
2023-08-23 03:57:20 +0100

    Update manual_rps.py

--- computer-vision-rock-paper-scissors/manual_rps.py
+++ computer-vision-rock-paper-scissors/manual_rps.py
@@ -1,31 +1,41 @@
 import random
 
-options = ['rock', 'paper', 'scissors']
+
+# options = ['rock', 'paper', 'scissors']
 # cpu_choice = random.randint(1, 3)
 # print("Options: ")
 # [print(f"{i}: {j.capitalize()}") for i, j in enumerate(options, 1)]
 # user_choice = int(input("Enter the number corresponding to your choice: "))
 
-def get_cpu_choice():
-    cpu_choice = random.randint(1, 3)
-    return cpu_choice
+class RockPaperScissors:
+    def __init__(self, num_wins=0):
+        self.num_wins = num_wins
+        self.cpu_choice = None
+        self.user_choice = None
+        self.options = ['rock', 'paper', 'scissors']
 
-def get_user_choice():
-    print("Options: ")
-    [print(f"{i}: {j.capitalize()}") for i, j in enumerate(options, 1)]
-    user_choice = int(input("Enter the number corresponding to your choice: "))
-    return user_choice
+    def get_cpu_choice(self):
+        self.cpu_choice = random.randint(1, 3)
 
-def get_winner():
-    if user_choice > cpu_choice and user_choice != 3:
-        print("You win!")
-    elif user_choice == cpu_choice:
-        print("It's a draw!")
-    else:
-        print("You lose.")
+    def get_user_choice(self):
+        print("Options: ")
+        [print(f"{i}: {j.capitalize()}") for i, j in enumerate(self.options, 1)]
+        self.user_choice = int(input("Enter the number corresponding to your choice: "))
+
+    def get_winner(self):
+        if self.user_choice > self.cpu_choice and self.user_choice != 3:
+            print("You win!")
+        elif self.user_choice == self.cpu_choice:
+            print("It's a draw!")
+        else:
+            print("You lose.")
+
+    def play(self):
+        self.get_cpu_choice()
+        self.get_user_choice()
+        self.get_winner()
+        print(f"User: {self.options[self.user_choice-1].capitalize()}, CPU: {self.options[self.cpu_choice-1].capitalize()}")
 
 
-cpu_choice = get_cpu_choice()
-user_choice = get_user_choice()
-print(f"User: {options[user_choice].capitalize()}, CPU: {options[cpu_choice].capitalize()}")
-get_winner()
+p = RockPaperScissors()
+p.play()
